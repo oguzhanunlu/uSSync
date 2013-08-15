e96dcd31932fe86de0fc0f869c9c916c27a4712a
Oğuzhan Ünlü <cengoguzhanunlu@gmail.com>
2013-08-16 00:30:20 +0300

    Created First UI

---
+++ gui/dev_main.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setEnabled(True)
+        MainWindow.resize(474, 369)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setFocusPolicy(QtCore.Qt.TabFocus)
+        MainWindow.setAcceptDrops(True)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.toolButton = QtGui.QToolButton(self.centralwidget)
+        self.toolButton.setGeometry(QtCore.QRect(430, 60, 23, 25))
+        self.toolButton.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.toolButton.setPopupMode(QtGui.QToolButton.MenuButtonPopup)
+        self.toolButton.setObjectName(_fromUtf8("toolButton"))
+        self.toolButton_2 = QtGui.QToolButton(self.centralwidget)
+        self.toolButton_2.setGeometry(QtCore.QRect(430, 120, 23, 25))
+        self.toolButton_2.setPopupMode(QtGui.QToolButton.MenuButtonPopup)
+        self.toolButton_2.setObjectName(_fromUtf8("toolButton_2"))
+        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(120, 60, 301, 27))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.lineEdit_2 = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(120, 120, 301, 27))
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 60, 91, 21))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 120, 91, 21))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.progressBar = QtGui.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(30, 270, 421, 21))
+        self.progressBar.setProperty("value", 24)
+        self.progressBar.setObjectName(_fromUtf8("progressBar"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(30, 240, 121, 21))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.line = QtGui.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(30, 220, 421, 20))
+        self.line.setFrameShape(QtGui.QFrame.HLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 190, 61, 27))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(370, 190, 71, 27))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(190, 160, 98, 27))
+        self.pushButton_3.setDefault(True)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 474, 25))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuFile = QtGui.QMenu(self.menubar)
+        self.menuFile.setObjectName(_fromUtf8("menuFile"))
+        self.menuHelp = QtGui.QMenu(self.menubar)
+        self.menuHelp.setObjectName(_fromUtf8("menuHelp"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionCtrl_O = QtGui.QAction(MainWindow)
+        self.actionCtrl_O.setObjectName(_fromUtf8("actionCtrl_O"))
+        self.actionOpen_2 = QtGui.QAction(MainWindow)
+        self.actionOpen_2.setObjectName(_fromUtf8("actionOpen_2"))
+        self.menuFile.addAction(self.actionOpen_2)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "uSSync Test App", None))
+        self.toolButton.setText(_translate("MainWindow", "...", None))
+        self.toolButton_2.setText(_translate("MainWindow", "...", None))
+        self.label.setText(_translate("MainWindow", "   Source", None))
+        self.label_2.setText(_translate("MainWindow", "  Destination", None))
+        self.label_3.setText(_translate("MainWindow", "Progress", None))
+        self.pushButton.setText(_translate("MainWindow", "Save ", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Cancel", None))
+        self.pushButton_3.setText(_translate("MainWindow", "Sync Now", None))
+        self.menuFile.setTitle(_translate("MainWindow", "File", None))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help", None))
+        self.actionCtrl_O.setText(_translate("MainWindow", "Ctrl+O", None))
+        self.actionOpen_2.setText(_translate("MainWindow", "Open", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
