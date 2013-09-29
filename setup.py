5fd6e7f9766b9e9ead6e36b5934649ad0364a2e2
Erdal Sivri <erdal@ceng.metu.edu.tr>
2013-09-30 01:19:58 +0300

    sync...

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='uSSync: A USB Sync Tool',
+      author='Erdal Sivri, Oguzhan Unlu',
+      author_email='erdal@ceng.metu.edu,tr',
+      url='',
+      packages=['uSSync'],
+)
+
